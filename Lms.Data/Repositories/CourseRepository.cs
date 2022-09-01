141821ac9e3ddb4d13b3abb95bac02c00a443542
emilk <emilks@gmail.com>
2022-09-01 17:05:53 +0200

    Repositories

---
+++ Lms.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lms.Core.Entities;
+using Lms.Core.Repositories;
+using Lms.Data.Data;
+
+namespace Lms.Data.Repositories
+{
+    internal class CourseRepository : ICourseRepository
+    {
+        private readonly LmsApiContext db = null!;
+
+        public CourseRepository(LmsApiContext db)
+        {
+            this.db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public void Add(Course course)
+        {
+            db.Course.Add(course);
+        }
+
+        public async Task<bool> AnyAsync(int? id)
+        {
+            return db.Course.Any(m => m.Id == id);
+        }
+
+        public async Task<Course> FindAsync(int? id)
+        {
+            return await db.Course.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Course>> GetAllCourses()
+        {
+            return db.Course.AsEnumerable();
+        }
+
+        public async Task<Course> GetCourse(int? id)
+        {
+            return await db.Course.FindAsync(id);
+        }
+
+        public void Remove(Course course)
+        {
+            db.Course.Remove(course);
+        }
+
+        public void Update(Course course)
+        {
+            db.Course.Update(course);
+        }
+    }
+}
---
+++ Lms.Data/Repositories/ModuleRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lms.Core.Entities;
+using Lms.Core.Repositories;
+using Lms.Data.Data;
+
+namespace Lms.Data.Repositories
+{
+    internal class ModuleRepository: IModuleRepository
+    {
+        private readonly LmsApiContext db = null!;
+
+        public ModuleRepository(LmsApiContext db)
+        {
+            this.db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public void Add(Module module)
+        {
+            db.Module.Add(module);
+        }
+
+        public async Task<bool> AnyAsync(int? id)
+        {
+            return db.Module.Any(m => m.Id == id);
+        }
+
+        public async Task<Module> FindAsync(int? id)
+        {
+            return await db.Module.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Module>> GetAllModules()
+        {
+            return db.Module.AsEnumerable();
+        }
+
+        public async Task<Module> GetModule(int? id)
+        {
+            return await db.Module.FindAsync(id);
+        }
+
+        public void Remove(Module module)
+        {
+            db.Module.Remove(module);
+        }
+
+        public void Update(Module module)
+        {
+            db.Module.Update(module);
+        }
+    }
+}
